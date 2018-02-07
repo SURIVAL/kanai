0c1868444714f47bbad3197de36059b4504e249e
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-02-07 16:21:53 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter tne number check for mulitple of 7");
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("mulitple of 7");
+}
+else
+{
+printf("not mulitple of 7");
+}
+}
