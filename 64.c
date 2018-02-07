f16822a166ff0bbf31ebe494378a073e5b27242d
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-02-07 16:29:20 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,m;
+printf("enter the two number");
+scanf("%d %d",&n,&m);
+if(n%2==0)
+{
+printf("even number");
+}
+else
+{
+printf("odd number");
+}
+}
+
