4a06fd75a03fa3c226e7f0a62f978629793e2cfe
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-02-07 16:22:35 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int x,y,z;
+printf("enter the two number");
+scanf("%d %d",&x,&y);
+z=x-y;
+if(z%2==0)
+{
+printf("even number");
+}
+else
+{
+printf("odd number");
+}
+}
