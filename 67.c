36522d936bc4c1f94c71d3dc02d08a8ef41e3271
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-02-07 16:21:14 +0530

    Create 67.c

---
+++ 67.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter tne number check the nearest gearter mulitple of 10");
+scanf("%d",&n);
+if(n%10==0)
+{
+printf("mulitple of 10");
+}
+else
+{
+printf("not mulitple of 10");
+}
+}
